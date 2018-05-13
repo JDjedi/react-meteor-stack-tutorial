1d361a0baab4da2ad0a7006ad89b1e172fa2627f
JDjedi <jcdiaz1201@gmail.com>
2018-05-13 16:41:45 -0500

    9.0 complete

--- imports/client/App.js
+++ imports/client/App.js
@@ -6,6 +6,30 @@ import Items from '../api/items'; // this is the file that makes the collection 
 
 
 class App extends React.Component {
+
+	addItems(event) {
+		event.preventDefault(); // prevents default funtionality, e.g. page refresh on hiting submit in the form below
+		const itemOne = this.refs.itemOne.value.trim();
+		const itemTwo = this.refs.itemTwo.value.trim();
+
+		if (itemOne != '' && itemTwo != '') {
+			Items.insert({
+				itemOne: {
+					text: itemOne,
+					value: 0
+				},
+
+				itemTwo: {
+					text: itemTwo,
+					value: 0
+				}
+			})
+
+			this.refs.itemOne.value = ''; // after insert this clears the input text box
+			this.refs.itemTwo.value = '';
+		}
+	}
+
 	render() {
 		return (
 			<div>
@@ -13,9 +37,14 @@ class App extends React.Component {
 					<h1>Hello World!</h1>
 				</header>
 				<main>
+					<form onSubmit={this.addItems.bind(this)}> 
+						<input type='text' ref='itemOne'/>
+						<input type='text' ref='itemTwo'/>
+						<button type='submit'>Add Items</button>
+					</form>
 					<p>This is the paragraph</p>
 					{this.props.items.map((item) => { // this funtion iterated through the array from the item.js in the api folder
-						return <Item item={item} /> // you're pullint the Item from the import statement above in ./item
+						return <Item item={item} key={item._id}/> // you're pullint the Item from the import statement above in ./item
 					})}
 				</main>
 			</div>
--- server/main.js
+++ server/main.js
@@ -3,15 +3,5 @@ import { Meteor } from 'meteor/meteor';
 import Items from '../imports/api/items'; // this is in the API folder!
 
 Meteor.startup(() => {
-	Items.insert({
-		itemOne: {
-			text: 'U.S.',
-			value: 0
-		},
 
-		itemTwo: {
-			text: 'China',
-			value: 0
-		}
-	});
 });
